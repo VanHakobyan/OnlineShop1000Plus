206bf64d78ec13e3cac6ee6b6059534ea943922e
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-11-20 19:40:29 +0400

    api fix

---
+++ OnlineShop/OnlineShop.Api/Controllers/CustomBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Api.Controllers
+{
+    [Route("api/[controller]/[action]/{id?}")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+    }
+}
---
+++ OnlineShop/OnlineShop.Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Api.Controllers
+{
+    public class TestController : CustomBaseController
+    {
+        public IActionResult Test()
+        {
+            return Ok($"Test {DateTime.UtcNow}");
+        }
+    }
+}
