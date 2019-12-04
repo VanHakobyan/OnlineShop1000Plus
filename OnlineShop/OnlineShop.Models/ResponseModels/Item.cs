e0ebd88b9030aeba5d3bc6dfe0a718e50d6a0366
lusine duryan <lusineduryan@mail.ru>
2019-12-04 15:49:54 +0400

    response models added

---
+++ OnlineShop/OnlineShop.Models/ResponseModels/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Common.ResponseModels
+{
+    public class Item
+    {
+        public int? Color { get; set; }
+        public int? Size { get; set; }
+        public int? Quantity { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ OnlineShop/OnlineShop.Models/ResponseModels/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Common.ResponseModels
+{
+    public class User
+    {
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email Address")]
+        [Required]
+        public string Email { get; set; }
+    }
+}
