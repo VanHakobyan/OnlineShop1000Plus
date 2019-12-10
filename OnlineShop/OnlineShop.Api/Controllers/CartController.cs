41151c86fcb2c79cd165d62ee231155726a04490
lusine duryan <lusineduryan@mail.ru>
2019-12-10 19:13:00 +0400

    minor changes on cart controller

--- OnlineShop/OnlineShop.Api/Controllers/CartController.cs
+++ OnlineShop/OnlineShop.Api/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Api.Services.Interfaces;
 using OnlineShop.Common;
@@ -7,6 +8,7 @@ using Serilog;
 
 namespace OnlineShop.Api.Controllers
 {
+    [Authorize]
     public class CartController : CustomBaseController
     {
         private readonly ICartService _cartService;
@@ -17,6 +19,11 @@ namespace OnlineShop.Api.Controllers
             _itemsService = itemsService;
         }
 
+        /// <summary>
+        /// shows the cart of the user
+        /// </summary>
+        /// <param name="userId">id of the user</param>
+        /// <returns>cart of the user</returns>
         [HttpGet]
         [ProducesDefaultResponseType]
         public IActionResult Cart([FromQuery(Name = "userId")] int userId)
@@ -37,6 +44,12 @@ namespace OnlineShop.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// adds an item to the cart
+        /// </summary>
+        /// <param name="userId">id of the user</param>
+        /// <param name="itemId">id of the item to be added to the cart</param>
+        /// <returns>resulted cart</returns>
         [HttpPost]
         [ProducesDefaultResponseType]
         public IActionResult AddToCart([FromQuery(Name = "userId")] int userId, [FromQuery(Name = "itemId")] int itemId)
@@ -45,7 +58,7 @@ namespace OnlineShop.Api.Controllers
             {
                 IEnumerable<Cart> cart = _cartService.AddItemToCart(userId, itemId);
                 var item = _itemsService.GetById(itemId);
-                if (item.Quantity == 0)
+                if (item != null & item.Quantity == 0)
                 {
                     return NotFound("Item expired!");
                 }
@@ -62,6 +75,12 @@ namespace OnlineShop.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// removes an item from the cart
+        /// </summary>
+        /// <param name="userId">id of the user></param>
+        /// <param name="itemId">id of the item to be removed</param>
+        /// <returns>resulted cart</returns>
         [HttpDelete]
         [ProducesDefaultResponseType]
         public IActionResult Remove([FromQuery(Name = "userId")] int userId, [FromQuery(Name = "itemId")] int itemId)
